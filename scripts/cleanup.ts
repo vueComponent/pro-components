b837158f07d6d53f96cabaa213e640d840c7da2f
Sendya <18x@loacg.com>
2020-12-02 22:19:11 +0800

    fix: script & tsconfig

--- scripts/cleanup.ts
+++ scripts/cleanup.ts
@@ -1,30 +1,9 @@
-import * as path from 'path';
-import * as fs from 'fs-extra';
+const rimraf = require('rimraf');
+const path = require('path');
 
-function rm(distName: string, suffix?: string[], ignores?: string[]) {
-  const distPath = path.join(__dirname, '../', distName);
-  if (!fs.existsSync(distPath)) {
-    return;
-  }
-
-  const files = fs.readdirSync(distPath);
-
-  files.forEach(name => {
-    if (Array.isArray(ignores)) {
-      if (ignores.includes(name)) {
-        return;
-      }
-    }
-    if (Array.isArray(suffix)) {
-      if (suffix.find(s => name.endsWith(s))) {
-        fs.removeSync(path.join(distPath, name));
-      }
-    }
-  });
+const errorHandler = (err) => {
+  console.error('err', err);
 }
 
-// ls | grep -v .jest.js | grep -v .eslintrc.js | grep -e '.d.ts' -e '.js$' | xargs rm
-rm('./', ['.d.ts', '.js'], ['.eslintrc.js', '.jest.js', 'postcss.config.js']);
-
-// rm ./es/icons/*.d.ts
-rm('./es/icons/', ['.d.ts']);
+rimraf(path.resolve(__dirname, 'es'), errorHandler);
+rimraf(path.resolve(__dirname, 'lib'), errorHandler);
