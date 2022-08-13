1b5eb56609c8310dffc46c4b04656c8a145d6d91
li <li@lideMacBook-Air.local>
2022-08-13 11:46:47 +0800

    feat: add pro-form

---
+++ packages/pro-form/examples/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue';
+import ProLayout, { PageContainer } from '@ant-design-vue/pro-layout';
+import App from './App.vue';
+import router from './router';
+import icons from './icons';
+
+// functional
+import 'ant-design-vue/es/result/style';
+import 'ant-design-vue/es/button/style';
+import 'ant-design-vue/es/message/style';
+import 'ant-design-vue/dist/antd.dark.less';
+import '@ant-design-vue/pro-layout/dist/style.css';
+
+const app = createApp(App);
+
+app.use(router);
+
+app.use(ProLayout).use(PageContainer).use(icons).mount('#app');
