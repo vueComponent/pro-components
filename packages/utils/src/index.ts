e0b446a0ef422727c8599f312e7443e8d76c0251
Hello Li <1261142605@qq.com>
2022-08-30 10:43:38 +0800

    feat: add utils typing

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -4,3 +4,23 @@ import isUrl from './isUrl';
 
 export * from './is';
 export { isImg, isNil, isUrl };
+
+// type
+import {
+  VueNode,
+  ProFieldFCMode,
+  VueText,
+  ProFieldTextType,
+  RequestOptionsType,
+  ProFieldRequestData,
+  ProFieldValueObjectType,
+} from './typings';
+export {
+  VueNode,
+  ProFieldFCMode,
+  VueText,
+  ProFieldTextType,
+  RequestOptionsType,
+  ProFieldRequestData,
+  ProFieldValueObjectType,
+};
