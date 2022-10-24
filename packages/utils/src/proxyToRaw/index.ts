afa169457df902e98f9dbe3ade8026f391eb6c4e
sunshineLixun <1261142605@qq.com>
2022-10-24 19:07:18 +0800

    fix: 代码优化

--- packages/utils/src/proxyToRaw/index.ts
+++ packages/utils/src/proxyToRaw/index.ts
@@ -1,4 +1,5 @@
 import { toRaw, isProxy } from 'vue';
+import { cloneDeep } from 'lodash-es';
 import type { Recordable } from '../typings';
 
 export const proxyToRaw = (proxy: Recordable) => {
@@ -6,7 +7,7 @@ export const proxyToRaw = (proxy: Recordable) => {
   for (const key in proxy) {
     let value = proxy[key];
     if (isProxy(value)) {
-      value = toRaw(value);
+      value = cloneDeep(toRaw(value));
     }
     target[key] = value;
   }
